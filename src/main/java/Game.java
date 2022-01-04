02179f7466545a18f03c60358ee28a838a012ecb
arif <hossaininfo@ymail.com>
2022-01-04 04:08:46 +0100

    rolling20TimesWithNoPinsKnockedDownShouldReturnScoreZero test method created

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,3 +1,10 @@
 public class Game {
-    
+
+    public void roll(int i) {
+
+    }
+
+    public int score() {
+        return 0;
+    }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,5 +1,20 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class GameTest {
-    Game game = new Game();
+
+    @Test
+    void rolling20TimesWithNoPinsKnockedDownShouldReturnScoreZero() {
+//        arrange del
+        Game game = new Game();
+//        act del
+        for (int i = 0; i < 20; i++) {
+            game.roll(0);
+        }
+
+//        assert del
+        assertEquals(0, game.score());
+
+    }
 }
