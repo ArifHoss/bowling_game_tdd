4786a2249083e6fd44d537c3822b735504511aa2
arif <hossaininfo@ymail.com>
2022-01-04 16:00:25 +0100

    spareTest method created

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,12 +1,18 @@
 public class Game {
-
-    private int totalScore = 0;
+    private int[] rolls = new int[20];
+    private int currentRoll = 0;
 
     public void roll(int pinsKnockedDown) {
-        totalScore += pinsKnockedDown;
+        rolls[currentRoll++] = pinsKnockedDown;
     }
 
     public int score() {
-        return totalScore;
+        int score = 0;
+        for (int i = 0; i < rolls.length; i++) {
+            score += rolls[i];
+            if (i % 2 == 0 && rolls[i] + rolls[i + 1] == 10)
+                score += rolls[i + 2];
+        }
+        return score;
     }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -12,19 +12,29 @@ public class GameTest {
 
     @Test
     void rolling20TimesWithNoPinsKnockedDownShouldReturnScoreZero() {
-        multipleRolls(0);
+        multipleRolls(20, 0);
         assertEquals(0, game.score());
 
     }
 
     @Test
     void rolling20TimesWithOnePinsKnockedDownShouldReturnScore20() {
-        multipleRolls(1);
+        multipleRolls(20, 1);
         assertEquals(20, game.score());
     }
 
-    private void multipleRolls(int pinsKnockedDown) {
-        for (int i = 0; i < 20; i++) {
+    @Test
+    void spareTest(){
+        game.roll(5);
+        game.roll(5);
+        game.roll(2);
+        multipleRolls(17,0);
+        assertEquals(14,game.score());
+    }
+
+
+    private void multipleRolls(int rolling, int pinsKnockedDown) {
+        for (int i = 0; i < rolling; i++) {
             game.roll(pinsKnockedDown);
         }
     }
